5506bc590c4e419fe60bada499cd1cb24dbf3055
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2014-01-17 12:05:01 -0500

    make libs

--- SOURCECODE/root/src/root_test.cxx
+++ SOURCECODE/root/src/root_test.cxx
@@ -3,40 +3,40 @@
 
 int main(int argc, char const *argv[])
 {
-	std::string tree_name(argv[2]);
-	std::string file(argv[1]);
-	std::cout << "file: " << file << std::endl;
-	std::cout << "tree: " << tree_name << std::endl;
+    std::string tree_name(argv[2]);
+    std::string file(argv[1]);
+    std::cout << "file: " << file << std::endl;
+    std::cout << "tree: " << tree_name << std::endl;
 
-	root::tree_reader D;
+    root::tree_reader D;
 
-	D.add_file(file, tree_name);
+    D.add_file(file, tree_name);
 
-	std::cout << "There are " << D.size() << " entries." << std::endl;
-
-
-	D.set_branch("pt", root::double_precision);
-	D.set_branch("ip3d_pb", root::double_precision);
-	D.set_branch("ip3d_pu", root::double_precision);
-	D.set_branch("ip3d_pc", root::double_precision);
-	D.set_branch("eta", root::double_precision);
-	D.set_branch("subMaxSecondaryVertexRho", root::double_precision);
-	D.set_branch("MV1", root::double_precision);
-	D.set_branch("jfit_nvtx", root::integer);
-	
-
-	for (int i = 0; i < 100; ++i)
-	{
-		std::vector<double> v = D[i];
-
-		for (auto &entry : v)
-		{
-			std::cout << entry << std::setw(20);
-		}
-		std::cout << "\n";
-	}
+    std::cout << "There are " << D.size() << " entries." << std::endl;
 
 
+    D.set_branch("pt", root::double_precision);
+    D.set_branch("ip3d_pb", root::double_precision);
+    D.set_branch("ip3d_pu", root::double_precision);
+    D.set_branch("ip3d_pc", root::double_precision);
+    D.set_branch("eta", root::double_precision);
+    D.set_branch("subMaxSecondaryVertexRho", root::double_precision);
+    D.set_branch("MV1", root::double_precision);
+    D.set_branch("jfit_nvtx", root::integer);
+    
 
-	return 0;
+    for (int i = 0; i < 100; ++i)
+    {
+        std::vector<double> v = D[i];
+
+        for (auto &entry : v)
+        {
+            std::cout << entry << std::setw(20);
+        }
+        std::cout << "\n";
+    }
+
+
+
+    return 0;
 }
