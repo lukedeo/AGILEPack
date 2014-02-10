1df146d22cb394585f8dea899d42cdaa0cb1f695
Luke de Oliveira <lukepercivaldeoliveira@hotmail.com>
2014-02-10 14:10:20 -0500

    working on interface

--- src/interface.cxx
+++ src/interface.cxx
@@ -1,6 +1,8 @@
 // #include "Base"
 #include "include/parser.hh"
 
+// void config_info(); 
+
 
 //----------------------------------------------------------------------------
 int main(int argc, char const *argv[])
@@ -11,10 +13,10 @@ int main(int argc, char const *argv[])
     optionparser::parser p(s + " models on ROOT TTrees");
 
     p.add_option("--file", "-f").help("Pass at least one file to add to a TChain for training.")
-                                .required(true)
+                                .required(false)
                                 .mode(optionparser::store_mult_values);
 
-    p.add_option("--save", "-s").help("Name of file to save the YAML config to.")
+    p.add_option("--save", "-s").help("Name of file to save the YAML neural network file to.")
                                 .required(false)
                                 .mode(optionparser::store_value);
 
@@ -24,16 +26,38 @@ int main(int argc, char const *argv[])
     p.add_option("--momentum")  .help("Pass a learning rate.")
                                 .mode(optionparser::store_value);
 
-    p.add_option("--load")      .help("Name of a YAML config file to load to begin training")
+    p.add_option("--load")      .help("Name of a YAML neural network file to load to begin training")
                                 .mode(optionparser::store_value);
+
+    std::string config_help = "Pass a configuration file for training specifications instead\n";
+    config_help.append(25, ' ');
+    config_help += "of over the command line. For help on formatting, pass\n";
+    config_help.append(25, ' ');
+    config_help += "the '-confighelp' flag. This overrides all paramaters from CLI.";
+
+    p.add_option("--config", "-c")  .help(config_help)
+                                    .mode(optionparser::store_value);
+    
+    p.add_option("-confighelp") .help("Display info about YAML training config files.");
+
+    std::string struct_help = "Pass the structure of the neural network. For example, one\n";
+    struct_help.append(25, ' ');
+    struct_help += "could specify --struct=13 24 23 15 3 [other flags].";
+
+    p.add_option("--struct")    .help(struct_help)
+                                .mode(optionparser::store_mult_values);
+
+    
 
     p.eat_arguments(argc, argv);
 
 
 
-
-
-
-
     return 0;
 }
+
+
+// void config_info()
+// {
+
+// }
