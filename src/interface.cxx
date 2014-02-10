338b2bf727dd3c877b9e75128587b4b856a01ed8
Luke de Oliveira <lukepercivaldeoliveira@hotmail.com>
2014-02-10 15:59:34 -0500

    working on interace

--- src/interface.cxx
+++ src/interface.cxx
@@ -13,21 +13,20 @@ int main(int argc, char const *argv[])
     optionparser::parser p(s + " models on ROOT TTrees");
 
 //----------------------------------------------------------------------------
-    p.add_option("--file", "-f").help("Pass at least one file to add to a TChain for training.")
-                                .required(false)
-                                .mode(optionparser::store_mult_values);
+    p.add_option("--file", "-f")    .help("Pass at least one file to add to a TChain for training.")
+                                    .mode(optionparser::store_mult_values);
 //----------------------------------------------------------------------------
-    p.add_option("--save", "-s").help("Name of file to save the YAML neural network file to.")
-                                .mode(optionparser::store_value);
+    p.add_option("--save", "-s")    .help("Name of file to save the YAML neural network file to.")
+                                    .mode(optionparser::store_value);
 //----------------------------------------------------------------------------
-    p.add_option("--learning")  .help("Pass a learning rate.")
-                                .mode(optionparser::store_value);
+    p.add_option("--learning")      .help("Pass a learning rate.")
+                                    .mode(optionparser::store_value);
 //----------------------------------------------------------------------------
-    p.add_option("--momentum")  .help("Pass a learning rate.")
-                                .mode(optionparser::store_value);
+    p.add_option("--momentum")      .help("Pass a learning rate.")
+                                    .mode(optionparser::store_value);
 //----------------------------------------------------------------------------
-    p.add_option("--load")      .help("Name of a YAML neural network file to load to begin training")
-                                .mode(optionparser::store_value);
+    p.add_option("--load")          .help("Name of a YAML neural network file to load to begin training")
+                                    .mode(optionparser::store_value);
 //----------------------------------------------------------------------------
     std::string config_help = "Pass a configuration file for training specifications instead\n";
     config_help.append(25, ' ');
@@ -38,14 +37,14 @@ int main(int argc, char const *argv[])
     p.add_option("--config", "-c")  .help(config_help)
                                     .mode(optionparser::store_value);
 //----------------------------------------------------------------------------
-    p.add_option("-confighelp") .help("Display info about YAML training config files.");
+    p.add_option("-confighelp")     .help("Display info about YAML training config files.");
 //----------------------------------------------------------------------------
     std::string struct_help = "Pass the structure of the neural network. For example, one\n";
     struct_help.append(25, ' ');
     struct_help += "could specify --struct=13 24 23 15 3 [other flags].";
 
-    p.add_option("--struct")    .help(struct_help)
-                                .mode(optionparser::store_mult_values);
+    p.add_option("--struct")        .help(struct_help)
+                                    .mode(optionparser::store_mult_values);
 //----------------------------------------------------------------------------
     std::string autoencoder_help = "Pretraining using deep autoencoders, if you pass an\n";
     autoencoder_help.append(25, ' ');
@@ -55,7 +54,29 @@ int main(int argc, char const *argv[])
                                     .mode(optionparser::store_value)
                                     .default_value("-1");
 //----------------------------------------------------------------------------
+    p.add_option("--verbose", "-v") .help("Make the output verbose");
+//----------------------------------------------------------------------------
+    p.add_option("-start")          .help("Start index for training. (Default = 0)")
+                                    .mode(optionparser::store_value)
+                                    .default_value("0");
+//----------------------------------------------------------------------------
+    p.add_option("-end")            .help("End index for training. (Default, whole tree)")
+                                    .mode(optionparser::store_value)
+                                    .default_value("-1");  
+//----------------------------------------------------------------------------
+    p.add_option("-epochs")         .help("Number of passes over the Trees. (Default = 10)")
+                                    .mode(optionparser::store_value)
+                                    .default_value("10");
+//----------------------------------------------------------------------------
+    std::string prog_string = "Output progress files every (n) epochs. If n isn't passed,\n";
+    prog_string.append(25, ' ');
+    p.add_option("-prog")           .help(prog_string + "uses default. (Default = 1)")
+                                    .mode(optionparser::store_value)
+                                    .default_value("1");
+//----------------------------------------------------------------------------
     p.eat_arguments(argc, argv);
+
+
 
     return 0;
 }
