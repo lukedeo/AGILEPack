361c3062b77369681caa73049562bae6ae76ee9a
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-02-22 14:01:56 -0500

    cleaned junk

--- root/src/tree_reader.cxx
+++ root/src/tree_reader.cxx
@@ -117,7 +117,7 @@ void tree_reader::set_branches(const std::string &yamlfile)
         YAML::Node config = tmp["branches"];
         std::map<std::string, std::string> vars = 
         config.as<std::map<std::string, std::string>>();
-        
+
         for (auto &entry : vars)
         {
             auto type = entry.second;
@@ -151,7 +151,8 @@ std::map<std::string, std::string> tree_reader::get_var_types()
     return variable_type_map;
 }
 //----------------------------------------------------------------------------
-agile::dataframe tree_reader::get_dataframe(int entries, int start, bool verbose)
+agile::dataframe tree_reader::get_dataframe(int entries, int start, 
+    bool verbose)
 {
     if ((entries > (int)m_size) || ((start + entries) > (int)m_size))
     {
@@ -216,7 +217,8 @@ double tree_reader::operator()(const unsigned int &idx, std::string col_name)
     return storage.at(traits[col_name].pos)->get_value<double>();
 }
 
-std::map<std::string, double> tree_reader::operator()(const unsigned int &idx, const std::vector<std::string> &names)
+std::map<std::string, double> tree_reader::operator()(const unsigned int &idx, 
+    const std::vector<std::string> &names)
 {
     m_smart_chain->GetEntry(idx);
     std::map<std::string, double> map;
@@ -227,7 +229,6 @@ std::map<std::string, double> tree_reader::operator()(const unsigned int &idx, c
     return std::move(map);
 
 }
-// std::vector<double> tree_reader::operator[](std::string col_name);
 
 //-----------------------------------------------------------------------------
 //  Information
