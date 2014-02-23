64771404f8854f7cccb83d2215c7774e3a32cbfb
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-02-23 14:01:24 -0500

    finished binner

--- root/include/binner.hh
+++ root/include/binner.hh
@@ -26,34 +26,40 @@ public:
 	void set_bins(const std::vector<double> &v);
 
 	template <typename T>
-	double get_bin(const std::map<std::string, T> &map);
+	int get_bin(const std::map<std::string, T> &map);
 
 	template <typename T>
-	double get_bin(const T& var);
+	int get_bin(const T& var);
+
+	template <typename T>
+	bool in_range(const std::map<std::string, T> &map);
+
+	template <typename T>
+	bool in_range(const T& var);
 
 	~binner();
 
 private:
-	double find_bin(const double &val);
+	int find_bin(const double &val);
 	std::string m_name;
 	std::vector<double> m_bins;
+	bool m_bins_present;
+	bool m_name_present;
 	// binner m_sub_binner;
 
 };
 
 //----------------------------------------------------------------------------
-//----------------------------------------------------------------------------
-binner::binner(const std::string &name, const std::initializer_list<double> &il)
+binner::binner(const std::string &name, 
+	const std::initializer_list<double> &il)
 : m_name(name), m_bins(il) {}
 //----------------------------------------------------------------------------
-
 binner::binner(const std::string &name, const std::vector<double> &v)
 : m_name(name), m_bins(v) {}
 //----------------------------------------------------------------------------
 binner::binner(const std::string &name)
 : m_name(name) {}
 //----------------------------------------------------------------------------
-
 binner::binner() 
 : m_name("") {}
 //----------------------------------------------------------------------------
@@ -77,7 +83,7 @@ void binner::set_bins(const std::vector<double> &v)
 //----------------------------------------------------------------------------
 
 template <typename T>
-double binner::get_bin(const std::map<std::string, T> &map)
+int binner::get_bin(const std::map<std::string, T> &map)
 {
 	if (!std::is_arithmetic<T>::value)
 	{
@@ -96,13 +102,53 @@ double binner::get_bin(const std::map<std::string, T> &map)
 //----------------------------------------------------------------------------
 
 template <typename T>
-double binner::get_bin(const T& var)
+int binner::get_bin(const T& var)
 {
 	if (!std::is_arithmetic<T>::value)
 	{
 		throw std::domain_error("invalid type passed to binner.");
 	}
 	return find_bin(dynamic_cast<double>(var));
+}
+//----------------------------------------------------------------------------
+
+template <typename T>
+bool binner::in_range(const std::map<std::string, T> &map)
+{
+	if (!std::is_arithmetic<T>::value)
+	{
+		throw std::domain_error("invalid type passed to binner.");
+	}
+	try
+	{
+		double val = dynamic_cast<double>(map.at(m_name));
+		if ((val >= m_bins.front()) && (val <= m_bins.back()))
+		{
+			return true;
+		}
+		return false;
+	}
+	catch(std::out_of_range &e)
+	{
+		throw std::out_of_range("no key named \'" + 
+			m_name +  "\' found in map.");
+	}
+}
+//----------------------------------------------------------------------------
+
+template <typename T>
+bool binner::in_range(const T& var)
+{
+	if (!std::is_arithmetic<T>::value)
+	{
+		throw std::domain_error("invalid type passed to binner.");
+	}
+	double val = dynamic_cast<double>(var);
+	if ((val >= m_bins.front()) && (val <= m_bins.back()))
+	{
+		return true;
+	}
+	return false;
 }
 //----------------------------------------------------------------------------
 
@@ -113,14 +159,20 @@ binner::~binner()
 //----------------------------------------------------------------------------
 
 
-double binner::find_bin(const double &val)
+int binner::find_bin(const double &val)
 {
-	
+	double lower_bound = m_bins.front();
+	for (unsigned int i = 1; i < m_bins.size(); ++i)
+	{
+		if((val >= lower_bound) && (val < m_bins[i]))
+		{
+			return dynamic_cast<int>(i - 1);
+		}
+	}
+	throw std::out_of_range("value = " + std::to_string(val) + 
+		" outside of binning range.")
 }
 //----------------------------------------------------------------------------
-
-
-
 
 }
 
