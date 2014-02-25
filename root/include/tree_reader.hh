0de1f97696ee3a3c974d390568a3a93ecf51c5ff
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-02-25 14:41:26 -0500

    create a vector of binned vars

--- root/include/tree_reader.hh
+++ root/include/tree_reader.hh
@@ -95,6 +95,9 @@ private:
     std::map<std::string, std::string> variable_type_map;
     typedef __INTERNAL::numeric_handler number_container;
     std::vector<std::unique_ptr<number_container>> storage;
+
+
+    bool m_binned_present;
     std::vector<std::string> feature_names, binned_names;
 
     std::map<std::string, agile::root::binner> m_binned_vars;
