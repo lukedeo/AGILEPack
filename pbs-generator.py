9587a98ea9f7cd6b0051bcd750cf061cabf44299
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-03-13 16:14:31 -0700

    working on variadic generator

--- pbs-generator.py
+++ pbs-generator.py
@@ -7,21 +7,29 @@ class PBS_Script(object):
 		super(PBS_Script, self).__init__()
 		self.executable = executable
 		self.addtl_args = list()
+		self.variadic = {}
 
 
-	def set_executable(executable):
+	def set_executable(self, executable):
 		if not isinstance(executable, str):
 			raise TypeError('Must pass the name of an excecutable as a string')
 		self.executable = executable
 
-	def add_static_arg(argument):
+	def add_static_arg(self, argument):
 		if not isinstance(argument, str):
 			raise TypeError("Must pass arguments as strings, a la foo.add_static_arg('--file=myfile.txt')")
 		self.addtl_args.append(argument)
 
+	def add_variadic_arg(self, arg_dict):
+		if not isinstance(arg_dict, dict):
+			raise TypeError("Must pass variadic args as a dictionary")
+		self.variadic.update(arg_dict)
+
+
+# for tagger, tagger_specs in taggerdict.iteritems():		
 
 
 
-		
 
-		
+
+
