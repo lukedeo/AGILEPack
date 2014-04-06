aad45f2d05b41a8d413eb571dd0ae7f5eeea43d6
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-04-06 19:18:13 -0400

    basic loading complete

--- python/agilepy.py
+++ python/agilepy.py
@@ -1,4 +1,5 @@
 import yaml
+import numpy as np
 
 def sigmoid(x):
 	return 1 / (1+ np.exp(-x))
@@ -7,15 +8,41 @@ def softmax(x):
 	tmp = np.exp(x)
 	return tmp / np.sum(tmp)
 
+def identity(x):
+	return x
+
 
 def _scale(arr, scaling):
 	for key in scaling['mean'].keys():
 		arr[key] = (arr[key] - scaling['mean'][key]) / scaling['sd'][key]
 	return arr
 
+def _destringify(nrow, ncol, string):
+	M = np.zeros(shape=(nrow, ncol))
+	data = [float(num) for num in string[(string.find(',') + 1):].split(',')]
+	for i in range(0, nrow):
+		M[i, :] = data[ (i * ncol) : (i * ncol + ncol)]
+	return M
+
+def _layer_from_yaml(d):
+	W = _destringify(d['outputs'], d['inputs'], d['weights'])
+	b = _destringify(d['outputs'], 1, d['bias'])
+
+	if d['activation'] == 'sigmoid':
+		f = sigmoid
+	elif d['activation'] == 'linear':
+		f = identity
+	elif d['activation'] == 'softmax':
+		f = softmax
+	else:
+		raise LookupError("activation function type \'{}\' not yet supported.".format(d['activation']))
+	return [W, b, f]
+
 
 class Layer(object):
-	'''Layer class which will hold W, b, and activation function'''
+	'''
+	Layer class which will hold W, b, and activation function
+	'''
 	def __init__(self, parms = [None, None, None]):
 		super(Layer, self).__init__()
 		self.W, self.b, self.activation = parms
@@ -24,7 +51,7 @@ class Layer(object):
 		self.W, self.b, self.activation = parms
 
 	def _fire(self, M):
-		return self.activation(self.W * M + b)
+		return self.activation(self.W * M + self.b)
 		
 
 		
@@ -43,7 +70,7 @@ class NeuralNet(object):
 		self.has_outputs = False
 		self.has_binning = False
 
-	def load(filename):
+	def load(self, filename):
 		with open(filename, 'r') as f:
 			y = yaml.load(f.read())
 
@@ -67,7 +94,15 @@ class NeuralNet(object):
 			self.outputs = y['network']['target_order']
 			self.has_targets = True
 
-		self.architecture = [Layer(parameters) for parameters in ]
+		self.architecture = [Layer(_layer_from_yaml(y['network'][idx])) for idx in y['network']['layer_access']]
+
+	def predict(self, data):
+		data = data.T
+		for layer in self.architecture:
+			data = layer._fire(data)
+		return data
+
+
 
 
 
