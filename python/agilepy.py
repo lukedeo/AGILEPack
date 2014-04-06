3796cf3fa4e6b2d7eae4acd88a56235bd094f335
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-04-06 13:51:12 -0400

    working on python client

---
+++ python/agilepy.py
@@ -0,0 +1,8 @@
+class NeuralNet(object):
+	'''
+	NeuralNet: a class for holding a serialized network estimated using AGILEPackj
+	'''
+	def __init__(self, arg):
+		super(NeuralNet, self).__init__()
+		self.arg = arg
+		
