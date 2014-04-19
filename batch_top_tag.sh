e243dd6d45c27eb6f49666b102e10aa7983d491d
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-04-19 11:27:15 -0400

    minor

--- batch_top_tag.sh
+++ batch_top_tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #PBS -t 1-3
-#PBS -l mem=4gb,walltime=00:03:00:00
+#PBS -l mem=16gb,walltime=00:010:00:00
 #PBS -l nodes=1:ppn=1
 #PBS -q hep
 #PBS -o output/out.txt
@@ -16,8 +16,7 @@ mkdir -p output
 echo 'submitted from: ' $PBS_O_WORKDIR 
 
 
-
-./AGILETopTagger --file=~/scratch_space/mc12_8TeV_JZX_Zprime_perfntuple_shuffled.root --tree=top_train_ntup --save=AGILETopTagger"$(echo -n "${formulas[$PBS_ARRAYID-1]}" | openssl dgst -sha1 -hmac "key")".yaml --learning=0.00086 --momentum=0.8 --batch=1 --momentum=0.8 --config=./top_tag_config.yaml -uepochs=10 -sepochs=30 -start=0 -end=6000 --type=binary --formula=${formulas[$PBS_ARRAYID-1]} --struct=4 5 3 2 1
+./AGILETopTagger --file=~/scratch_space/mc12_8TeV_JZX_Zprime_perfntuple_shuffled.root --tree=top_train_ntup --save=AGILETopTagger"$(echo -n "${formulas[$PBS_ARRAYID-1]}" | openssl dgst -sha1 -hmac "key")".yaml --learning=0.00086 --momentum=0.8 --batch=1 --momentum=0.8 --config=./top_tag_config.yaml -uepochs=10 -sepochs=30 -start=0 -end=1671000 --type=binary --formula=${formulas[$PBS_ARRAYID-1]} --struct=4 5 3 2 1
 
 
 
